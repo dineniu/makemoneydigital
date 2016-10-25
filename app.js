1ee580c5674ddf1c17997075c0f6f6b28e751c2a
Claudinei <dine_niu@hotmail.com>
2016-10-25 21:36:00 -0200

    string 1

--- app.js
+++ app.js
@@ -12,8 +12,34 @@ Genre = require('./models/genre');
 Book = require('./models/book');
 
 //Connect to DB
-//mongoose.connect('mongodb://admin:212230@jello.modulusmongo.net:27017/e5rEqazi');
-mongoose.connect(OPENSHIFT_MONGODB_DB_URL + OPENSHIFT_APP_NAME);
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
+}
+
+mongoose.connect(mongoURL);
+//mongoose.connect(OPENSHIFT_MONGODB_DB_URL + OPENSHIFT_APP_NAME);
 var db = mongoose.connection;
 
 app.get('/',function(req, res){
@@ -58,5 +84,4 @@ app.get('/api/books/:_id',function(req, res){
 	});
 });
 
-var port = process.env.PORT || 8080;
 app.listen(port);
