a825b45cf73e44ec39dd58e6417b8e84811a4b5a
dineniu <dine_niu@hotmail.com>
2017-01-24 13:53:52 -0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-var express    = require('express'),
-    http       = require('http');
-    
+var express    = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var path = require('path');
-Solvemedia = require('solvemedia');
 
 
 var publicDir = path.join(__dirname, 'public')
 app.use(bodyParser.json());
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('ejs', require('ejs-locals'));
 
 Genre = require('./models/genre');
 Book = require('./models/book');
@@ -23,41 +16,6 @@ Book = require('./models/book');
 mongoose.connect('mongodb://usuario:usuario123@172.30.165.103:27017/testedb');
 //mongoose.connect(OPENSHIFT_MONGODB_DB_URL + OPENSHIFT_APP_NAME);
 var db = mongoose.connection;
-
-app.get('/(validate)?', function(req, res) {
-
-    var solvemedia = new Solvemedia('PUBLIC_KEY','PRIVATE_KEY', 'AUTHENTICATION_KEY');
-    res.render('register', {
-        layout: false,
-        locals: {
-            name        : '',
-            captcha     : solvemedia.toHTML(), 
-            errorMessage: ''
-                
-        }
-    });
-});
-
-app.post('/validate', function(req, res) {
-    var solvemedia = new Solvemedia('PUBLIC_KEY','PRIVATE_KEY', 'AUTHENTICATION_KEY');
-    
-    solvemedia.verify(req.body.adcopy_response,req.body.adcopy_challenge, req.connection.remoteAddress, function(isValid,errorMessage){
-        if (isValid) {
-            res.send('Hi ' + req.body.name + ', Solvemedia told me that you are not a robot!!');
-        } else {
-            // Redisplay the form.
-            res.render('register', {
-                layout: false,
-                locals: {
-                    name        : req.body.name,
-                    captcha     : solvemedia.toHTML(),                  
-                    errorMessage: errorMessage
-                }
-            });                            
-        }
-    });
-});        
-
 
 app.get('/',function(req, res){
 	 res.sendFile(path.join(publicDir, 'index.html'));
