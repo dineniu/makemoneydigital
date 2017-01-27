5562eb8b0daa2ad8b7acb1cba78062101b573b01
dineniu <dine_niu@hotmail.com>
2017-01-26 22:57:49 -0200

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.get('/',function(req, res){
 	var lang = req.query.lang;
         var obj = fs.readFileSync(path.join(publicDir, 'index.html'), 'utf8');
 	if(lang === "pt"){
-	   obj = obj.replace("./verify", "./verificar").replace("lang: 'en'", "lang: 'pt'").replace("SEND CAPTCHA","ENVIAR CAPTCHA");
+	   obj = "Em Portugues";
+	   //obj = obj.replace("./verify", "./verificar").replace("lang: 'en'", "lang: 'pt'").replace("SEND CAPTCHA","ENVIAR CAPTCHA");
 	}
 	 res.send(obj);
 });
