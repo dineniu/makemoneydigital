40ad9cfcaf76db9522aaaedbfaab484113156997
dineniu <dine_niu@hotmail.com>
2017-01-26 23:06:30 -0200

    Update app.js

--- app.js
+++ app.js
@@ -20,17 +20,16 @@ mongoose.connect('mongodb://admin:212230@jello.modulusmongo.net:27017/e5rEqazi')
 var db = mongoose.connection;
 
 app.get('/',function(req, res){
-	var lang = req.query.lang;
         var obj = fs.readFileSync(path.join(publicDir, 'index.html'), 'utf8');
-	if(lang == "pt"){
+	if(req.session.pt){
 	   obj = "Em Portugues";
 	   //obj = obj.replace("./verify", "./verificar").replace("lang: 'en'", "lang: 'pt'").replace("SEND CAPTCHA","ENVIAR CAPTCHA");
 	}
 	 res.send(obj);
 });
 app.get('/pt',function(req, res){
-	var string = encodeURIComponent('pt');
-        res.redirect('/?lang='+string);
+	req.session.pt = true;
+        res.redirect('/);
 });
 
 app.post('/verificar',function(req, res){
