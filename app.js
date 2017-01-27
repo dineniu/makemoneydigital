35bfe52fba998a7e3242d726ce108b9260be5202
dineniu <dine_niu@hotmail.com>
2017-01-26 23:33:15 -0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var path = require('path');
