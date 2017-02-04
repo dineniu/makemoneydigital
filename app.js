1b78dcf95ff47c502eb7e3f16b993d2227fa3afd
Claudinei <dine_niu@hotmail.com>
2017-02-04 16:10:24 -0200

    app config limpo

--- app.js
+++ app.js
@@ -1,104 +1,26 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 var path = require('path');
 var fs = require('fs');
-//var cors = require('cors');
 
 var publicDir = path.join(__dirname, 'public')
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//app.use(cors());
-
-Genre = require('./models/genre');
-Book = require('./models/book');
-SolveMedia = require('./solvemedia');
-//Connect to DB
-mongoose.connect('mongodb://admin:212230@jello.modulusmongo.net:27017/e5rEqazi');
-var db = mongoose.connection;
 
 app.get('/',function(req, res){
-        var obj = fs.readFileSync(path.join(publicDir, 'index.html'), 'utf8');
+    var obj = fs.readFileSync(path.join(publicDir, 'index.html'), 'utf8');
 	if(req.query.lang == "pt"){
-	  obj = obj.replace("./verify", "./verificar").replace("lang: 'en'", "lang: 'pt'").replace("SEND CAPTCHA","ENVIAR CAPTCHA");
+	  
 	}
 	 res.send(obj);
 });
 app.get('/pt',function(req, res){
 	var string = encodeURIComponent('pt');
-  res.redirect('/?lang=' + string);
+    res.redirect('/?lang=' + string);
 });
 
-app.post('/verificar',function(req, res){
-	var solvemedia = new SolveMedia('HdETkCNNkpqCIuBAU90dEO4CjZn.5UpT','-6Hb8iRfq3yvLH9Rr80uobcOqswPpMcZ', 'YJzbSgW5YN0b8ECv455mmYoD6Oosza9K');
-	solvemedia.verify(req.body.adcopy_response,req.body.adcopy_challenge, req.connection.remoteAddress, function(isValid,errorMessage){
-        if (isValid) {
-		res.redirect('http://preev.com/btc/brl');
-        } else {
-                var obj = fs.readFileSync(path.join(publicDir, 'index.html'), 'utf8');
-		obj = obj.replace("lang: 'en'", "lang: 'pt'").replace("display:none","display:block").replace("Answer captcha to go to CONVERT", "O captcha esta incorreto, tente novamente.");
-	 	res.send(obj);
-        }
-    });
-});
-app.post('/verify',function(req, res){
-	var solvemedia = new SolveMedia('HdETkCNNkpqCIuBAU90dEO4CjZn.5UpT','-6Hb8iRfq3yvLH9Rr80uobcOqswPpMcZ', 'YJzbSgW5YN0b8ECv455mmYoD6Oosza9K');
-	solvemedia.verify(req.body.adcopy_response,req.body.adcopy_challenge, req.connection.remoteAddress, function(isValid,errorMessage){
-        if (isValid) {
-            res.redirect('http://preev.com/');
-        } else {
-            var obj = fs.readFileSync(path.join(publicDir, 'index.html'), 'utf8');
-		obj = obj.replace("display:none","display:block").replace("Answer captcha to go to CONVERT", "O captcha esta incorreto, tente novamente.");
-	 	res.send(obj);
-        }
-    });
-});
-
-
-
-app.get('/bitcoin',function(req, res){
-	 res.sendFile(path.join(publicDir, 'bitcoin.html'));
-});
-
-app.get('/api/genres',function(req, res){
-	Genre.getGenres(function(err, genres){
-		if(err){
-			throw err;
-		}
-		res.json(genres);
-	});
-});
-
-app.post('/api/genres',function(req, res){
-	var genre = req.body;
-
-	Genre.addGenres(genre, function(err, genre){
-		if(err){
-			throw err;
-		}
-		res.json(genre);
-	});
-});
-
-app.get('/api/books',function(req, res){
-	Book.getBooks(function(err, books){
-		if(err){
-			throw err;
-		}
-		res.json(books);
-	});
-});
-
-app.get('/api/books/:_id',function(req, res){
-	Book.getBookById(req.params._id,function(err, book){
-		if(err){
-			throw err;
-		}
-		res.json(book);
-	});
-});
 
 var port = process.env.PORT || 8080;
 app.listen(port);
