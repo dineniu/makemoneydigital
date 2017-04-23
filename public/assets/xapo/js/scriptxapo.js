e2bf9fd2d2eef495ed6843b976fd375ad04a8a75
dineniu <dine_niu@hotmail.com>
2017-04-23 14:10:54 -0300

    Update scriptxapo.js

--- public/assets/xapo/js/scriptxapo.js
+++ public/assets/xapo/js/scriptxapo.js
@@ -1,20 +1,14 @@
-var moon = 5*60;
-var map = {};
-$('#btnMoonBitcoin').on('click', function(){
-	//if(moon == 5*60)
-	//	countdown(moon, 5*60, '#spnMoonBitcoin');
-});
-
 $(".item-button button").on('click',function(){
   var minutos = parseInt($(this).parent().find('span').text().split(' ')[1].split(':')[0]);
   if(minutos){
     var segundos = minutos * 60;
     var constante = minutos * 60;
-    //minutos $(this).parent().find('span').attr("id")
-    countdown(segundos,constante,$(this).parent().find('span').attr("id"));
+    var link = $(this).find("a");
+     link.hide();
+    countdown(segundos,constante,$(this).parent().find('span').attr("id"), link);
   }
 });
-function countdown(variavel, constante, span){
+function countdown(variavel, constante, span,link){
 	if(variavel != 0){
 		variavel--;
 		var minuto = parseInt(variavel/60);
@@ -22,9 +16,10 @@ function countdown(variavel, constante, span){
 		var segundo = variavel%60;
 		segundo = segundo < 10 ? "0"+segundo: segundo;
 		$("#"+span).html("Time: "+minuto+":"+segundo);
-		setTimeout(function(){countdown(variavel, constante, span)}, 1000);
+		setTimeout(function(){countdown(variavel, constante, span, link)}, 1000);
 	}
 	else{
 		variavel = constante;
+		$(link).show();
 	}
 }
