3a84e505bb16663f869892d8d2a60561cbda6305
dineniu <dine_niu@hotmail.com>
2017-04-23 13:51:45 -0300

    Update scriptxapo.js

--- public/assets/xapo/js/scriptxapo.js
+++ public/assets/xapo/js/scriptxapo.js
@@ -1,17 +1,18 @@
 var moon = 5*60;
-
+var map = {};
 $('#btnMoonBitcoin').on('click', function(){
 	//if(moon == 5*60)
 	//	countdown(moon, 5*60, '#spnMoonBitcoin');
 });
 
 $(".item-button button").on('click',function(){
-var minutos = parseInt($(this).parent().find('span').text().split(' ')[1].split(':')[0]);
-	if(minutos){alert(minutos);
-	
-	}else{
-	alert("Nao!");
-	}
+  var minutos = parseInt($(this).parent().find('span').text().split(' ')[1].split(':')[0]);
+  if(minutos){
+    var segundos = minutos * 60;
+    var constante = minutos * 60;
+    //minutos $(this).parent().find('span').attr("id")
+    countdown(segundos,constante,$(this).parent().find('span').attr("id"));
+  }
 });
 function countdown(variavel, constante, span){
 	if(variavel != 0){
